a2e18e557567fad4c43044e079d40120be00387d
Vanya1000 <Vanya1000@yandex.ru>
2023-01-19 17:51:44 +0300

    feat: implement ws server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { httpServer } from "./http_server/index.js";
+import { WebSocketServer, createWebSocketStream } from "ws";
+import "dotenv/config";
+import router from "./router/index.js";
+
+const HTTP_PORT: number = Number(process.env.HTTP_PORT) || 8181;
+const WS_PORT: number = Number(process.env.WS_PORT) || 8080;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: WS_PORT }, () =>
+  console.log(`Start web socket server on the ${WS_PORT} port!`)
+);
+
+wss.on("connection", function connection(ws) {
+  ws.on("close", () => console.log("[Server] Client disconnected."));
+
+  console.log("New connection!");
+
+
+});
+
+process.on("SIGINT", () => {
+  wss.close();
+  console.log("\nWeb Socket Server closed.");
+  process.exit(0);
+});
