f9d816d00d1b79993987943aefba0d4a390c3e76
Vanya1000 <Vanya1000@yandex.ru>
2023-01-21 18:47:53 +0300

    refactor: clean comments

--- src/actions/printScreen.ts
+++ src/actions/printScreen.ts
@@ -6,23 +6,17 @@ export const printScreen = async (subCMD: string) => {
     try {
       const { x, y } = await mouse.getPosition();
       const region = new Region(x, y, 200, 200);
-      // await screen.highlight(region);
       const nutImage = await screen.grabRegion(region);
       const { data, width, height } = await nutImage.toRGB();
-      // console.log(nutImage);
       const jimpImage = new Jimp({
         data,
         width,
         height,
       });
-
-      // console.log(jimpImage);
       const imgBase64 = await jimpImage.getBase64Async(Jimp.MIME_PNG);
-      // console.log(imgBase64);
       return `${"prnt_scrn"} ${imgBase64.split(",").at(-1)}`;
     } catch (error) {
       return "The_screenshot_goes_beyond_the_boundaries_of_the_main_monitor";
     }
   }
 };
-// refactor this code
